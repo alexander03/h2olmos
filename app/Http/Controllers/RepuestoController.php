60487bfce7e035e15c3a523b773af77904f07d02
Stalyn <espacherrezpuyen@gmail.com>
2020-06-24 00:37:07 -0500

    CRUD repuesto finalizado

--- app/Http/Controllers/RepuestoController.php
+++ app/Http/Controllers/RepuestoController.php
@@ -122,4 +122,86 @@ class RepuestoController extends Controller
         });
         return is_null($error) ? "OK" : $error;
     }
+
+    public function edit($id, Request $request)
+    {
+        $existe = Libreria::verificarExistencia($id, 'repuesto');
+        if ($existe !== true) {
+            return $existe;
+        }
+        $listar   = Libreria::getParam($request->input('listar'), 'NO');
+        $repuesto = Repuesto::find($id);
+        $entidad  = 'Repuesto';
+        $formData = array('repuestos.update', $id);
+        $formData = array('route' => $formData, 'method' => 'PUT', 'class' => 'form-horizontal', 'id' => 'formMantenimiento'.$entidad, 'autocomplete' => 'off');
+        $boton    = 'Modificar';
+        $arrUnidades = Unidad::getAll();
+        $cboUnidades = array('' => 'Seleccione');
+        foreach($arrUnidades as $k=>$v){
+            $cboUnidades += array($v->id=>$v->descripcion);
+        }
+        return view($this->folderview.'.mant')->with(compact('repuesto', 'formData', 'entidad', 'boton', 'cboUnidades', 'listar'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $existe = Libreria::verificarExistencia($id, 'repuesto');
+        if ($existe !== true) {
+            return $existe;
+        }
+        $reglas     = array(
+            'codigo' => 'required',
+            'descripcion' => 'required|max:100',
+            'unidad_id' => 'required'
+        );
+        $mensajes = array(
+            'codigo.required' => 'Debe ingresar un código',
+            'codigo.min' => 'Código inválido',
+            'descripcion.required' => 'Debe ingresar una descripcion',
+            'unidad_id.required' => 'Debe seleccionar una unidad'
+        );
+        $validacion = Validator::make($request->all(), $reglas, $mensajes);
+        if ($validacion->fails()) {
+            return $validacion->messages()->toJson();
+        }
+        $error = DB::transaction(function() use($request, $id){
+            $repuesto = Repuesto::find($id);
+            $repuesto->codigo= strtoupper($request->input('codigo'));
+            $repuesto->descripcion= strtoupper($request->input('descripcion'));
+            $repuesto->unidad_id= strtoupper($request->input('unidad_id'));
+            $repuesto->save();
+        });
+        return is_null($error) ? "OK" : $error;
+    }
+
+    public function destroy($id)
+    {
+        $existe = Libreria::verificarExistencia($id, 'repuesto');
+        if ($existe !== true) {
+            return $existe;
+        }
+        $error = DB::transaction(function() use($id){
+            $brand = Repuesto::find($id);
+            $brand->delete();
+        });
+        return is_null($error) ? "OK" : $error;
+    }
+
+    public function eliminar($id, $listarLuego)
+    {
+        $existe = Libreria::verificarExistencia($id, 'repuesto');
+        if ($existe !== true) {
+            return $existe;
+        }
+        $listar = "NO";
+        if (!is_null(Libreria::obtenerParametro($listarLuego))) {
+            $listar = $listarLuego;
+        }
+        $mensaje=true;
+        $modelo   = Repuesto::find($id);
+        $entidad  = 'Repuesto';
+        $formData = array('route' => array('repuestos.destroy', $id), 'method' => 'DELETE', 'class' => 'form-horizontal', 'id' => 'formMantenimiento'.$entidad, 'autocomplete' => 'off');
+        $boton    = 'Eliminar';
+        return view('app.confirmarEliminar')->with(compact('modelo', 'formData', 'entidad', 'boton', 'listar','mensaje'));
+    }
 }
