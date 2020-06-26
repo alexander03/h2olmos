6bc008632e1ef7657f7f758657c90f5f8a697e0a
jazcprograming <joseabrahamzapatacruz@gmail.com>
2020-06-26 00:54:39 -0500

    AreaTrabajoContVN

--- resources/views/app/contratistas/mant.blade.php
+++ resources/views/app/contratistas/mant.blade.php
@@ -4,13 +4,13 @@
 <div class="form-group">
 	{!! Form::label('razonsocial', 'Razon Social:', array('class' => 'col-lg-12 col-md-12 col-sm-12 control-label')) !!}
 	<div class="col-lg-12 col-md-12 col-sm-12">
-		{!! Form::text('razonsocial', null, array('class' => 'form-control input-xs', 'id' => 'razonsocial', 'maxlength' => '100')) !!}
+		{!! Form::text('razonsocial', null, array('class' => 'form-control input-xs', 'id' => 'razonsocial', 'maxlength' => '150')) !!}
 	</div>
 </div>
 <div class="form-group">
 	{!! Form::label('ruc', 'RUC:', array('class' => 'col-lg-12 col-md-12 col-sm-12 control-label')) !!}
 	<div class="col-lg-12 col-md-12 col-sm-12">
-		{!! Form::text('ruc', null, array('class' => 'form-control input-xs', 'id' => 'ruc', 'maxlength' => '100')) !!}
+		{!! Form::text('ruc', null, array('class' => 'form-control input-xs', 'id' => 'ruc', 'maxlength' => '11')) !!}
 	</div>
 </div>
 <div class="form-group">
@@ -24,5 +24,13 @@
 	$(document).ready(function() {
 		configurarAnchoModal('350');
 		init(IDFORMMANTENIMIENTO+'{!! $entidad !!}', 'M', '{!! $entidad !!}');
+		const inputCode = document.getElementById('ruc');
+		inputCode.addEventListener('keydown', e => {
+			if (e.keyCode < 48|| (e.keyCode > 57&&e.keyCode < 96)
+			|| (e.keyCode > 105)){
+				if(e.keyCode!=8&&e.keyCode!=13&&e.keyCode!=37&&e.keyCode!=39)
+    			e.preventDefault();
+  			}
+  		})
 	}); 
 </script>
