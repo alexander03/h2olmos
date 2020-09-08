23471046a1bc4ce54b689f110bb153b1cd6ba626
unknown <cesar.787@hotmail.com>
2020-09-07 21:57:20 -0500

    modididicación controldiario

--- app/Http/Controllers/ControlDiarioController.php
+++ app/Http/Controllers/ControlDiarioController.php
@@ -63,7 +63,7 @@ class ControlDiarioController extends Controller
         }
 */
         $resultado        = Controldiario::whereHas('ua', function($query) use($filtro){
-        									$query->where($filtro[0][0],$filtro[0][1],$filtro[0][2]);
+        									$query->where($filtro[0][0],$filtro[0][1],$filtro[0][2])->where($filtro[3][0],$filtro[3][1],$filtro[3][2]);
         								})->orWhereHas('equipo', function($query) use($filtro){
         									$query->where($filtro[1][0],$filtro[1][1],$filtro[1][2])
         									->orWhere($filtro[2][0],$filtro[2][1],$filtro[2][2]);
