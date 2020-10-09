e7b17eb3d8ae7abd63558338ec7219de5d582dcd
Stalyn <espacherrezpuyen@gmail.com>
2020-10-09 12:05:37 -0500

    bitacora migrate created

---
+++ database/migrations/2020_10_09_073938_create_bitacora_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBitacoraTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bitacora', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('accion'); //CREATED | UPDATED | DELETED | RESTORED
+            $table->string('tabla');
+            $table->string('registro_id');
+            $table->string('valor_anterior');
+            $table->string('valor_posterior');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bitacora');
+    }
+}
